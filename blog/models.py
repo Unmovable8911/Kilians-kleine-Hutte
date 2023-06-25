0b1aa06db09d4f08f87b70b22cb63f03eee615a3
Unmovable8911 <jonas.tiedermann.97@proton.me>
2023-06-25 09:25:13 +0800

    update

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    cover = models.FileField(upload_to="media/blog_covers")
+    cover = models.FileField(upload_to="blog_covers")
     body = models.TextField()
     abstract = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
